9db163e5bfd99881dc38cb9a4d8242d13af826d8
Ramkrushna435 <vickymaneshwar435@gmail.com>
2024-06-23 16:43:01 +0530

    Wallet-app
    code changes

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -7,6 +7,19 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
+  features : any[] = [
+    {id: 1, name: 'Top Up', src:  '../../assets/icon/wallet.png', page:''},
+    {id: 2, name: 'Withdraw', src:  '../../assets/icon/cash-withdrawal.png', page:''},
+    {id: 3, name: 'Send', src:  '../../assets/icon/send.png', page:''},
+    {id: 4, name: 'Pay', src:  '../../assets/icon/pay-per-click.png', page:''}
+  ];
+
+  transaction : any[] = [
+    {id: 1, vendor: 'Pizza', image: '', amount: -1200 , time: '3:00pm'},
+    {id: 2, vendor: 'Received', image: '', amount: +200, time: '5:00am,'},
+    {id: 1, vendor: 'Movie Ticket', image: '', amount: -200, time: '9:00pm'},
+  ]
+
   constructor() {}
 
 }
